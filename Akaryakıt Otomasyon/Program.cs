4975806dcb77b0755523db0796172453411e1a05
Yaser Bin Humaidah <muratys8877@gmail.com>
2020-08-20 22:48:19 +0300

    tumunle isie

---
+++ Akaryakıt Otomasyon/Program.cs
@@ -0,0 +1,35 @@
+﻿/****************************************************************************
+**						 SAKARYA ÜNİVERSİTESİ
+**				BİLGİSAYAR VE BİLİŞİM BİLİMLERİ FAKÜLTESİ
+**				 BİLİŞİM SİSTEMLERİ MÜHENDİSLİĞİ BÖLÜMÜ 
+**				    NESNEYE DAYALI PROGRAMLAMA DERSİ
+**					     2019-2020 YAZ DÖNEMİ 
+**	
+**				PROJE NUMARASI.........: 01
+**				ÖĞRENCİ ADI............: YASER BİN HUMAİDAH 
+**				ÖĞRENCİ NUMARASI.......: B171200560
+**              DERSİN ALINDIĞI GRUP...: A
+****************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Akaryakıt_Otomasyon
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
---
+++ Akaryakıt Otomasyon/Properties/AssemblyInfo.cs
@@ -0,0 +1,50 @@
+﻿/****************************************************************************
+**						 SAKARYA ÜNİVERSİTESİ
+**				BİLGİSAYAR VE BİLİŞİM BİLİMLERİ FAKÜLTESİ
+**				 BİLİŞİM SİSTEMLERİ MÜHENDİSLİĞİ BÖLÜMÜ 
+**				    NESNEYE DAYALI PROGRAMLAMA DERSİ
+**					     2019-2020 YAZ DÖNEMİ 
+**	
+**				PROJE NUMARASI.........: 01
+**				ÖĞRENCİ ADI............: YASER BİN HUMAİDAH 
+**				ÖĞRENCİ NUMARASI.......: B171200560
+**              DERSİN ALINDIĞI GRUP...: A
+****************************************************************************/
+
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Akaryakıt Otomasyon")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Akaryakıt Otomasyon")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b2f9cef0-5c30-4cb5-abfe-1689d5ab0fd3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
